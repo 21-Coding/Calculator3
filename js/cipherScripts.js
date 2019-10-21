f8e78ea74b70b1b31a0434d1f67a2f78d87760e2
Dominique Youmans and Noah Horwitz <dominique@>
2019-10-21 14:28:11 -0700

    update cypher.

--- js/cipherScripts.js
+++ js/cipherScripts.js
@@ -1,21 +1,31 @@
 $(document).ready(function() {
+
+// PROMPT FOR SENTENCE
   var userSentence = prompt("Enter a sentence:");
   console.log(userSentence);
 
+// BEGIN CYPHER
+  // var test = function(){
+  //   userSentence.replace(/^./, userSentence.charAt(0).toUpperCase());
+  //   userSentence.replace(/.$/, userSentence.charAt(-0).toUpperCase());
+  //
+  //   alert(newSentence);
+  //   console.log(newSentence);
+  //
+  // };
+  // alert(test);
+  // console.log(test);
 
 
-  var test = function(){
-    newSentence = (userSentence.replace(/^./, userSentence.charAt(0).toUpperCase()));
-    alert(newSentence);
-    console.log(newSentence);
 
-  };
+  // WORKING OUTSIDE OF FUNCTION
+  var newSentence1 = userSentence.replace(/^./, userSentence.charAt(0).toUpperCase());
+  var newSentence2 = newSentence1.replace(/.$/, userSentence.charAt().toUpperCase());
 
-test
-alert(test);
-console.log(test);
-
-
-
+  alert(newSentence1);
+  console.log(newSentence1);
+  alert(newSentence2);
+  console.log(newSentence2);
+  // WORKING OUTSIDE OF FUNCTION
 
 });
